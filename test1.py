1d2cff75170cb69ea5afa76731d9e96eba4c007a
nicolatoscan <nicolatoscan99@outlook.it>
2020-11-12 01:55:57 +0100

    ciao

---
+++ test1.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+2 + 2
+
+2 + 3
+
+2 + 4
+
+3 + 3
+
+6 + 6
+
+23 + 23
+
+33 + 33
+
+#
+
+66 + 66
+
+120 + 120
