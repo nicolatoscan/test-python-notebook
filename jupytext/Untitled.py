18be215b27d38af09e577147aff591942be258d7
nicolatoscan <nicolatoscan99@outlook.it>
2020-11-12 02:02:46 +0100

    Riproviamoci ancora 2

---
+++ jupytext/Untitled.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+5 + 5
+
+# %%
+10 + 10
+
+# %%
